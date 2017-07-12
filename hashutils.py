5b17aafa0e96258238b13ab68ed0988248cdba20
Sean Stevens <sstevens88@gmail.com>
2017-07-11 20:16:20 -0700

    Fixed hashing functions

--- hashutils.py
+++ hashutils.py
@@ -2,18 +2,18 @@ import hashlib
 import string
 import random
 
+def make_salt():
+    return ''.join([random.choice(string.ascii_letters) for x in range(5)])
+
 def make_pw_hash(password, salt=None):
     if not salt:
         salt = make_salt()
     hashed = hashlib.sha256(str.encode(password + salt)).hexdigest()
-    return '{0}.{1}'.format(hash, salt)
+    return '{0}.{1}'.format(hashed, salt)
 
-def make_salt():
-    return ''.join([random.choice(string.ascii_letters) for x in range(5)])
-
-def check_pw_hash(password, salt):
+def check_pw_hash(password, hashed):
     salt = hashed.split('.')[1]
-    if make_pw_hash(password, salt) == hash:
+    if make_pw_hash(password, salt) == hashed:
         return True
 
     return False
